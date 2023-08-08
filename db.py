d6dbdb0a7d4549861fd1935d26f6fd7e058ead36
Taylor Etheredge <taylor.etheredge@gmail.com>
2023-08-08 15:30:08 -0500

    Adjusting the db.py file to use a var for the DB URI

--- db.py
+++ db.py
@@ -12,7 +12,9 @@ db_user = creds.get_db_user()
 db_host = creds.get_db_hostname()
 db_port = creds.get_db_port()
 
-DATABASE_URI = os.getenv('TEST_DB', f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
+db_uri = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+DATABASE_URI = os.getenv('TEST_DB', db_uri)
 engine = create_engine(DATABASE_URI, pool_size=2, isolation_level="AUTOCOMMIT")
 engine.dispose()
 Session = scoped_session(sessionmaker())
