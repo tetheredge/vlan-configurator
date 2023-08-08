44339a932c3272fd0f7fe4a5509e1b6c31461c90
Taylor Etheredge <taylor.etheredge@gmail.com>
2023-08-07 20:58:15 -0500

    Adding file to integrate with db and the creds needed

---
+++ db.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine
+
+import os
+import credentials
+from sqlalchemy.orm import sessionmaker, scoped_session
+from contextlib import contextmanager
+
+creds = credentials.Credentials()
+db_name = creds.get_db_name()
+db_password = creds.get_db_password()
+db_user = creds.get_db_user()
+db_host = creds.get_db_hostname()
+db_port = creds.get_db_port()
+
+DATABASE_URI = os.getenv('TEST_DB', f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
+engine = create_engine(DATABASE_URI, pool_size=2, isolation_level="AUTOCOMMIT")
+engine.dispose()
+Session = scoped_session(sessionmaker())
+
+session = Session(bind=engine)
+
+@contextmanager
+def session_scope():
+    engine.dispose()
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except Exception:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+        engine.dispose()
